84cb2a465f776ba71e4e4a8d6773f4aee6bb8fc6
jmone <jmone@jmone-pc.(none)>
2013-12-03 00:10:51 +0800

    e.sql
    Signed-off-by: jmone <jmone@jmone-pc.(none)>

--- docs/e.sql
+++ docs/e.sql
@@ -1,28 +1,32 @@
--- MySQL dump 10.13  Distrib 5.5.28, for Linux (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 3.4.8
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: e_shangqing
--- ------------------------------------------------------
--- Server version	5.5.28-log
+-- 主机: localhost
+-- 生成日期: 2013 年 12 月 03 日 00:02
+-- 服务器版本: 5.5.28
+-- PHP 版本: 5.3.17
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `buy_offer`
+-- 数据库: `e`
 --
 
-DROP TABLE IF EXISTS `buy_offer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `buy_offer` (
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `buy_offer`
+--
+
+CREATE TABLE IF NOT EXISTS `buy_offer` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `uid` int(10) NOT NULL,
   `name` varchar(20) NOT NULL,
@@ -32,71 +36,59 @@ CREATE TABLE `buy_offer` (
   `dateline` int(10) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `uid` (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='求购信息' AUTO_INCREMENT=5 ;
 
 --
--- Dumping data for table `buy_offer`
+-- 转存表中的数据 `buy_offer`
 --
 
-LOCK TABLES `buy_offer` WRITE;
-/*!40000 ALTER TABLE `buy_offer` DISABLE KEYS */;
-INSERT INTO `buy_offer` VALUES (1,1,'求购乐扣茶网水杯','',10,1374960391,1374765391);
-/*!40000 ALTER TABLE `buy_offer` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `buy_offer` (`id`, `uid`, `name`, `message`, `count`, `expiration_date`, `dateline`) VALUES
+(1, 1, 'test', 'test message.', 100, 1380390993, 1380390993),
+(2, 1, 'test', 'test message.', 100, 1380391001, 1380391001),
+(3, 1, 'test', 'test message.', 100, 1380391002, 1380391002),
+(4, 1, 'test', 'test message.', 100, 1380391014, 1380391014);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `common_user`
+-- 表的结构 `common_user`
 --
 
-DROP TABLE IF EXISTS `common_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `common_user` (
+CREATE TABLE IF NOT EXISTS `common_user` (
   `uid` int(10) NOT NULL,
   `name` int(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='普通用户';
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `common_user`
+-- 表的结构 `company_user`
 --
 
-LOCK TABLES `common_user` WRITE;
-/*!40000 ALTER TABLE `common_user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `common_user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `company_user`
---
-
-DROP TABLE IF EXISTS `company_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `company_user` (
+CREATE TABLE IF NOT EXISTS `company_user` (
   `uid` int(11) NOT NULL,
   `name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='企业用户';
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `company_user`
+-- 表的结构 `event`
 --
 
-LOCK TABLES `company_user` WRITE;
-/*!40000 ALTER TABLE `company_user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `company_user` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE IF NOT EXISTS `event` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` char(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `exhibition`
+-- 表的结构 `exhibition`
 --
 
-DROP TABLE IF EXISTS `exhibition`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `exhibition` (
+CREATE TABLE IF NOT EXISTS `exhibition` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `uid` int(10) NOT NULL,
   `name` varchar(20) NOT NULL COMMENT '展会名称',
@@ -107,26 +99,15 @@ CREATE TABLE `exhibition` (
   `dateline` int(10) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `uid` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='展会' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `exhibition`
+-- 表的结构 `friend_link`
 --
 
-LOCK TABLES `exhibition` WRITE;
-/*!40000 ALTER TABLE `exhibition` DISABLE KEYS */;
-/*!40000 ALTER TABLE `exhibition` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `friend_link`
---
-
-DROP TABLE IF EXISTS `friend_link`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `friend_link` (
+CREATE TABLE IF NOT EXISTS `friend_link` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `uid` int(10) NOT NULL,
   `name` varchar(20) NOT NULL,
@@ -136,26 +117,15 @@ CREATE TABLE `friend_link` (
   `dateline` int(10) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `uid` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='友情链接' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `friend_link`
+-- 表的结构 `message`
 --
 
-LOCK TABLES `friend_link` WRITE;
-/*!40000 ALTER TABLE `friend_link` DISABLE KEYS */;
-/*!40000 ALTER TABLE `friend_link` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `message`
---
-
-DROP TABLE IF EXISTS `message`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `message` (
+CREATE TABLE IF NOT EXISTS `message` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `from_uid` int(10) NOT NULL,
   `to_uid` int(10) NOT NULL,
@@ -164,26 +134,15 @@ CREATE TABLE `message` (
   `dateline` int(10) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `uid` (`from_uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='留言' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `message`
+-- 表的结构 `news`
 --
 
-LOCK TABLES `message` WRITE;
-/*!40000 ALTER TABLE `message` DISABLE KEYS */;
-/*!40000 ALTER TABLE `message` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `news`
---
-
-DROP TABLE IF EXISTS `news`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `news` (
+CREATE TABLE IF NOT EXISTS `news` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `uid` int(10) NOT NULL,
   `title` varchar(30) NOT NULL,
@@ -192,26 +151,15 @@ CREATE TABLE `news` (
   `dateline` int(10) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `uid` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='企业新闻' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `news`
+-- 表的结构 `position`
 --
 
-LOCK TABLES `news` WRITE;
-/*!40000 ALTER TABLE `news` DISABLE KEYS */;
-/*!40000 ALTER TABLE `news` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `position`
---
-
-DROP TABLE IF EXISTS `position`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `position` (
+CREATE TABLE IF NOT EXISTS `position` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `uid` int(10) NOT NULL,
   `name` varchar(20) NOT NULL COMMENT '职位名称',
@@ -221,26 +169,15 @@ CREATE TABLE `position` (
   `dateline` int(10) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `uid` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='职位' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `position`
+-- 表的结构 `sell_offer`
 --
 
-LOCK TABLES `position` WRITE;
-/*!40000 ALTER TABLE `position` DISABLE KEYS */;
-/*!40000 ALTER TABLE `position` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `sell_offer`
---
-
-DROP TABLE IF EXISTS `sell_offer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sell_offer` (
+CREATE TABLE IF NOT EXISTS `sell_offer` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `uid` int(10) NOT NULL,
   `name` varchar(100) NOT NULL COMMENT '商品名',
@@ -251,26 +188,15 @@ CREATE TABLE `sell_offer` (
   `dateline` int(10) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `uid` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='供应信息' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `sell_offer`
+-- 表的结构 `user`
 --
 
-LOCK TABLES `sell_offer` WRITE;
-/*!40000 ALTER TABLE `sell_offer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sell_offer` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `type` enum('1','2') NOT NULL DEFAULT '1' COMMENT '1 普通用户 2 企业用户',
   `email` varchar(100) NOT NULL,
@@ -279,26 +205,15 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   KEY `type` (`type`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='基础用户信息' AUTO_INCREMENT=2 ;
 
 --
--- Dumping data for table `user`
+-- 转存表中的数据 `user`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'1','jmone2006@126.com','32100321',1374760391);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `user` (`id`, `type`, `email`, `password`, `dateline`) VALUES
+(1, '1', 'jmone2006@126.com', '32100321', 1374760391);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-09-12 12:42:49
